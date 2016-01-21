9cc48b4138a17d0a8fc9d89524d62b21cfc65644
Adrian Miranda <adrian.miranda@isobar.com.br>
2016-01-20 22:34:04 -0200

    changing template engine

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+var nunjucks = require('nunjucks');
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname +'/public'));
+nunjucks.configure('views', {
+	autoescape:true,
+	express:app
+});
 
+app.set('port', (process.env.PORT || 5000));
+app.set('view engine', 'html');
 app.set('views', __dirname +'/views');
-app.set('view engine', 'ejs');
+app.use(express.static(__dirname +'/public'));
 
 app.get('/', function(request, response){
 	response.render('pages/main');
