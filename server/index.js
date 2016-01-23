f996cba7ef32c124f7ab5c7bf63367c18a55d7f6
Adrian Miranda <adrian.miranda@isobar.com.br>
2016-01-23 00:13:34 -0200

    update bodyParser methods

---
+++ server/index.js
@@ -0,0 +1,92 @@
+'use strict';
+
+// THIS_DIR = os.path.dirname(__file__);
+// ROOT_DIR = rel('../../');
+// BASE_DIR = rel('..');
+
+var ParseCloud = require('parse-cloud-express');
+var Parse = ParseCloud.Parse;
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var nunjucks = require('nunjucks');
+var express = require('express');
+var path = require('path');
+var environ = require('./utils/environ');
+var info = { url:{ base:path.resolve('.') } };
+var app = express();
+
+var Menu = {
+	'/flux':'Flux',
+	'/archives':'Archives',
+	'/contact':'Contact'
+};
+
+nunjucks.configure('views', {
+	autoescape:true,
+	express:app
+});
+
+
+//////////////////////////////////////////////////////////////
+// Configuration
+//////////////////////////////////////////////////////////////
+
+environ(environ.get('ENVRC', info.url.base +'/.environment'));
+environ.set('LOG_DIR_PATH', process.cwd(), true);
+app.set('port', environ.get('PORT', 3000));
+app.set('views', info.url.base +'/views');
+app.set('static', info.url.base +'/public');
+app.set('view engine', 'html');
+
+
+//////////////////////////////////////////////////////////////
+// Middleware
+//////////////////////////////////////////////////////////////
+
+app.use('/webhooks', ParseCloud.app);
+app.use(express.static(app.get('static')));
+app.use(bodyParser.urlencoded({ extended:false }));
+app.use(bodyParser.json);
+app.use(methodOverride());
+
+
+//////////////////////////////////////////////////////////////
+// Routes
+//////////////////////////////////////////////////////////////
+
+app.get('/', function(request, response){
+	response.render('index', { env:info, menu:Menu });
+});
+
+app.get('/flux/:project', function(request, response){
+	info.params = request.params;
+	response.render('pages/flux', { env:info });
+});
+
+app.get('/archives/:data', function(request, response){
+	info.params = request.params;
+	response.render('pages/archives', { env:info });
+});
+
+app.get('/contact', function(request, response){
+	response.render('pages/contact', { env:info });
+});
+
+app.all('*', function(request, response){
+	response.status(404).render('server/404');
+});
+
+app.use(function(error, request, response, next){
+	console.error(error.stack);
+	response.status(error.status || 500).render('server/'+error.status, {
+		env:info,
+		message:error.message,
+		error:error
+	});
+});
+
+app.listen(app.get('port'), function(){
+	var host = this.address().address;
+	var port = this.address().port;
+	console.log('Node app is running at http://%s:%s', host, port);
+});
