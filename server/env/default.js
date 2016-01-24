e2e7c3b136b70c53f9baf3070c5ae4385bba1deb
Adrian Miranda <adrian.miranda@isobar.com.br>
2016-01-24 00:41:54 -0200

    update defaults

--- server/env/default.js
+++ server/env/default.js
@@ -1,8 +1,15 @@
 /* global ambox */
 var environ = require('../utils/environ');
-ambox.uri('env.protocol', environ.get('NODE_ENV') === 'secure'? 'https' : 'http');
-ambox.uri('env.port', environ.get('PORT', 3000));
-ambox.uri('env.host', environ.get('HOST', '0.0.0.0'));
-ambox.uri('env.templateEngine', 'html');
-ambox.uri('env.port', 5000);
+ambox.uri('env.app.title', 'Ambox');
+ambox.uri('env.app.description', 'git-push powered folio');
+ambox.uri('env.app.keywords', 'ambox,node,heroku,express');
+ambox.uri('env.app.favicon', 'images/favicon.ico');
+ambox.uri('env.app.logo', 'images/share.jpg');
+ambox.uri('env.app.googleAnalytics.appId', '');
+ambox.uri('env.app.facebook.appId', '');
+ambox.uri('env.app.templateEngine', 'nunjucks');
+ambox.uri('env.url.protocol', environ.get('NODE_ENV') === 'secure'? 'https' : 'http');
+ambox.uri('env.url.port', environ.get('PORT', 3000));
+ambox.uri('env.url.host', environ.get('HOST', '0.0.0.0'));
+ambox.uri('env.url.server', ambox.env.url.protocol +'://'+ ambox.env.url.host +':'+ ambox.env.url.port);
 module.exports = ambox.uri('env');
