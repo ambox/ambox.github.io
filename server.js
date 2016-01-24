390a6c8fb405516680cc57502fbf4572a86b517a
Adrian Miranda <adrian.miranda@isobar.com.br>
2016-01-24 00:41:09 -0200

    removing cloud dependency

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 /* global global, ambox */
 var scope = require('./server/utils/scope').stub(global, 'ambox');
 var server = scope.uri('server', require('./server/index').create());
-var app = server.init();
+var app = server.init(onInitServer);
 server.start();
+
+function onInitServer(app){
+	
+	var Menu = {
+		'/flux':'Flux',
+		'/archives':'Archives',
+		'/contact':'Contact'
+	};
+	
+	app.get('/', function(request, response){
+		response.render('index', { menu:Menu });
+	});
+
+	app.get('/flux/:uid?', function(request, response){
+		response.render('pages/flux', { menu:Menu });
+	});
+
+	app.get('/archives/:uid?', function(request, response){
+		response.render('pages/archives', { menu:Menu });
+	});
+
+	app.get('/contact', function(request, response){
+		response.render('pages/contact', { menu:Menu });
+	});
+	
+}
