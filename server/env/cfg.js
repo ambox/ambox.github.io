5c4462b8a4f506296a937597b3f3029f365a6ba9
Adrian Miranda <adrian.miranda@isobar.com.br>
2016-01-25 10:54:54 -0200

    update cfg

--- server/env/cfg.js
+++ server/env/cfg.js
@@ -2,7 +2,7 @@
 
 try{
 	var environ = require('./environ');
-	var role = environ.get('NODE_ENV');
+	var role = environ.get('NODE_ENV', '');
 	switch(role.toLowerCase()){
 		case 'master':module.exports=require('./production');break;
 		case 'hml':module.exports=require('./staging');break;
