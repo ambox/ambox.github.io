7e971a879f321ab6ba33b080e048e3d928a43586
Adrian Miranda <adrian.miranda@isobar.com.br>
2016-01-27 11:49:17 -0200

    update data

--- server/hooks/archives.js
+++ server/hooks/archives.js
@@ -3,10 +3,12 @@ var Parse = require('parse/node');
 var Archive = Parse.Object.extend('Archive');
 var Tag = Parse.Object.extend('Tag');
 
-var menu = {
-	'/flux': 'Flux',
-	'/archives': 'Archives',
-	'/contact': 'Contact'
+var defaults = {
+	menu:{
+		'/flux': 'Flux',
+		'/archives': 'Archives',
+		'/contact': 'Contact'
+	}
 };
 
 var ArchivesCtrl = function(){
@@ -21,12 +23,12 @@ var ArchivesCtrl = function(){
 
 // Display a form for creating a new archive.
 ArchivesCtrl.prototype.renderNew = function(request, response){
-	response.render('partials/archives/new', { menu:menu });
+	response.render('partials/archives/new', defaults);
 };
 
 // Display a form for editing a specified archive.
 ArchivesCtrl.prototype.renderEdit = function(request, response){
-	var data = { menu:menu };
+	var data = defaults;
 	var query = new Parse.Query(Archive);
 	query.get(request.params.id).then(function(value){
 		if(value){
@@ -47,7 +49,7 @@ ArchivesCtrl.prototype.renderEdit = function(request, response){
 
 // Display all archives.
 ArchivesCtrl.prototype.findAll = function(request, response){
-	var data = { menu:menu };
+	var data = defaults;
 	var query = new Parse.Query(Archive);
 	query.descending('createdAt');
 	query.find().then(function(value){
@@ -63,7 +65,7 @@ ArchivesCtrl.prototype.findAll = function(request, response){
 
 // Show a given archive based on specified id.
 ArchivesCtrl.prototype.findOne = function(request, response){
-	var data = { menu:menu };
+	var data = defaults;
 	var query = new Parse.Query(Archive);
 	query.get(request.params.id).then(function(value){
 		data.archive = value;
@@ -79,7 +81,7 @@ ArchivesCtrl.prototype.findOne = function(request, response){
 
 // Create a new archive with specified title and body.
 ArchivesCtrl.prototype.create = function(request, response){
-	var data = { menu:menu };
+	var data = defaults;
 	var dto = ambox.pick(request.body, 'name', 'description');
 	var archive = new Archive();
 	archive.save(dto).then(function(){
@@ -94,7 +96,7 @@ ArchivesCtrl.prototype.create = function(request, response){
 
 // Update a archive based on specified id, title and body.
 ArchivesCtrl.prototype.update = function(request, response){
-	var data = { menu:menu };
+	var data = defaults;
 	var dto = ambox.pick(request.body, 'name', 'description');
 	var archive = new Archive();
 	archive.id = request.params.id;
@@ -110,7 +112,7 @@ ArchivesCtrl.prototype.update = function(request, response){
 
 // Delete a archive corresponding to the specified id.
 ArchivesCtrl.prototype.delete = function(request, response){
-	var data = { menu:menu };
+	var data = defaults;
 	var archive = new Archive();
 	archive.id = request.params.id;
 	// ...
