efb545d4a00345e24ea7bf8d7cc6397112617cec
Adrian Miranda <adrian.miranda@isobar.com.br>
2016-01-26 23:17:37 -0200

    update pack

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 /* global global, ambox */
 var caste = require('./server/caste').stub(global, 'ambox');
-var server = caste.uri('server', require('./server/index').create());
+var config = require('./server/env/cfg');
+var server = caste.uri('server', require('./server/index').create(config));
 var app = server.init(require('./server/urls'));
 server.start();
--- server/index.js
+++ server/index.js
@@ -13,15 +13,14 @@ var flash = require('connect-flash');
 var Parse = require('parse/node');
 var basicAuth = require('basic-auth');
 var environ = require('./env/environ');
-var cfg = require('./env/cfg');
-var app = express();
 
-var Server = function(options){
-	this.options = ambox.merge({}, options);
+var Server = function(cfg){
+	this.cfg = ambox.merge({}, cfg);
+	this.app = express();
 };
 
-Server.create = function(options){
-	return new Server(options);
+Server.create = function(cfg){
+	return new Server(cfg);
 };
 
 Server.basicAuth = function(username, password){
@@ -36,25 +35,25 @@ Server.basicAuth = function(username, password){
 };
 
 Server.prototype.initLocalVars = function(){
-	app.locals.title = ambox.uri('env.app.title');
-	app.locals.description = ambox.uri('env.app.description');
-	app.locals.keywords = ambox.uri('env.app.keywords');
-	app.locals.favicon = ambox.uri('env.app.favicon');
-	app.locals.logo = ambox.uri('env.app.logo');
-	app.locals.googleAnalyticsAppId = ambox.uri('env.service.googleAnalytics.appId');
-	app.locals.facebookAppId = ambox.uri('env.service.facebook.appId');
-	app.locals.server = ambox.uri('env.url.server');
+	this.app.locals.title = ambox.uri('env.app.title');
+	this.app.locals.description = ambox.uri('env.app.description');
+	this.app.locals.keywords = ambox.uri('env.app.keywords');
+	this.app.locals.favicon = ambox.uri('env.app.favicon');
+	this.app.locals.logo = ambox.uri('env.app.logo');
+	this.app.locals.googleAnalyticsAppId = ambox.uri('env.service.googleAnalytics.appId');
+	this.app.locals.facebookAppId = ambox.uri('env.service.facebook.appId');
+	this.app.locals.server = ambox.uri('env.url.server');
 };
 
 Server.prototype.initMiddleware = function(){
-	app.set('showStackError', true);
-	app.enable('jsonp callback');
-	app.use(compress({ filter:this.initCompress, level:9 }));
-	app.use(bodyParser.urlencoded({ extended:true }));
-	app.use(bodyParser.json());
-	app.use(methodOverride());
-	app.use(cookieParser());
-	app.use(flash());
+	this.app.set('showStackError', true);
+	this.app.enable('jsonp callback');
+	this.app.use(compress({ filter:this.initCompress, level:9 }));
+	this.app.use(bodyParser.urlencoded({ extended:true }));
+	this.app.use(bodyParser.json());
+	this.app.use(methodOverride());
+	this.app.use(cookieParser());
+	this.app.use(flash());
 };
 
 Server.prototype.initCompress = function(request, response){
@@ -64,19 +63,19 @@ Server.prototype.initCompress = function(request, response){
 };
 
 Server.prototype.initViewEngine = function(){
-	nunjucks.configure('views', { autoescape:true, express:app });
-	app.set('view engine', 'html');
-	app.set('views', ambox.uri('env.url.host') +'/views');
+	nunjucks.configure('views', { autoescape:true, express:this.app });
+	this.app.set('view engine', 'html');
+	this.app.set('views', ambox.uri('env.url.host') +'/views');
 };
 
 Server.prototype.initHeaders = function(){
 	var EIGHTEEN_WEAKS = 10886400000;
-	app.disable('x-powered-by');
-	app.use(helmet.xframe());
-	app.use(helmet.xssFilter());
-	app.use(helmet.nosniff());
-	app.use(helmet.ienoopen());
-	app.use(helmet.hsts({
+	this.app.disable('x-powered-by');
+	this.app.use(helmet.xframe());
+	this.app.use(helmet.xssFilter());
+	this.app.use(helmet.nosniff());
+	this.app.use(helmet.ienoopen());
+	this.app.use(helmet.hsts({
 		maxAge:EIGHTEEN_WEAKS,
 		includeSubdomains:true,
 		force:true
@@ -84,8 +83,8 @@ Server.prototype.initHeaders = function(){
 };
 
 Server.prototype.initStaticFiles = function(){
-	app.use('/', express.static(path.resolve('static')));
-	app.use(favicon('static/'+ app.locals.favicon));
+	this.app.use('/', express.static(path.resolve('static')));
+	this.app.use(favicon('static/'+ this.app.locals.favicon));
 };
 
 Server.prototype.initSession = function(){
@@ -103,13 +102,13 @@ Server.prototype.initModules = function(list){
 	list = Array.isArray(list)? list : [list];
 	list.forEach(function(module){
 		if(typeof module === 'function'){
-			module(app, auth);
+			module(this.app, auth);
 		}
-	});
+	}.bind(this));
 };
 
 Server.prototype.initErrorRoutes = function(){
-	app.use(function(error, request, response, next){
+	this.app.use(function(error, request, response, next){
 		if(!error){
 			return next();
 		}
@@ -127,18 +126,18 @@ Server.prototype.init = function(modules, callback){
 	this.initSession();
 	this.initModules(modules);
 	this.initErrorRoutes();
-	return app;
+	return this.app;
 };
 
 Server.prototype.start = function(callback){
 	var port = ambox.uri('env.url.port');
 	var host = ambox.uri('env.url.host');
-	app.listen(port, host, function(){
+	this.app.listen(port, host, function(){
 		console.log('['+ chalk.white(ambox.uri('env.app.title'))+ ']\n|');
 		console.log('|    '+chalk.green('Environment: ')+ environ.get('NODE_ENV', 'localhost'));
 		console.log('|    '+chalk.green('Server: ')+ ambox.uri('env.url.server'));
 		console.log('|_');
-		callback && callback(app, cfg);
+		callback && callback(this.app, this.cfg);
 	});
 };
 
