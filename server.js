406cf915cdecd8ea6e8d743396026057fbd84902
Adrian Miranda <adrian.miranda@isobar.com.br>
2016-01-26 23:18:46 -0200

    update pack

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-/* global global, ambox */
-var caste = require('./server/caste').stub(global, 'ambox');
+/* global global */
+var ambox = require('./server/caste').stub(global, 'ambox');
 var config = require('./server/env/cfg');
-var server = caste.uri('server', require('./server/index').create(config));
+var server = ambox.uri('server', require('./server/index').create(config));
 var app = server.init(require('./server/urls'));
 server.start();
