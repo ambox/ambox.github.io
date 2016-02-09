fab4788b89b91a952da1314a0b5fabf1688419cc
Adrian C. Miranda <adriancmiranda@gmail.com>
2016-02-09 10:22:08 -0200

    update dependencies

---
+++ tasks/open.js
@@ -0,0 +1,11 @@
+module.exports = function () {
+	'use strict'
+
+	// Open urls and files from a grunt task
+	// @see https://github.com/jsoverson/grunt-open
+	return {
+		localhost:{
+			path: 'http://0.0.0.0:3000'
+		}
+	}
+}
