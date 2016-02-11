01e7f3d9adcfb0eca07d472a675bfb37490621a9
Adrian C. Miranda <adriancmiranda@gmail.com>
2016-02-11 21:56:59 -0200

    clean removed

--- tasks/clean.js
+++
@@ -1,9 +0,0 @@
-module.exports = function () {
-	'use strict'
-
-	// Clean files and folders.
-	// @see https://github.com/gruntjs/grunt-contrib-clean
-	return {
-		tmp: '<%= scaffold.tmp %>/'
-	}
-}
